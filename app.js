d29956a42353575b50b517aac2a0038a7d74651a
Sneha Pote <Snehapote2016@gmail.com>
2021-03-20 18:35:34 +0530

    nodejs project first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const url ='mongodb://localhost/ProgrammerDBex'
+const port = process.env.PORT ||9000;
+const DB = 'mongodb+srv://sneha:Sneha20@cluster0.onku0.mongodb.net/ProgrammerDBex?retryWrites=true&w=majority';
+
+const app = express()
+
+mongoose.connect((DB),{useNewUrlParser:true,
+useCreateIndex:true,
+useUnifiedTopology:true,
+useFindAndModify:false}).then(()=>{
+    console.log('connection successful..');
+    }).catch((err) => console.log('no connection..'));
+
+//mongoose.connect('mongodb://localhost:27017/ProgrammerDBex', {useNewUrlParser:true},{useUnifiedTopology:true}); 
+/*
+const con = mongoose.connection 
+ 
+con.on('open',()=> {
+    console.log('connected...')
+})
+*/
+app.use(express.json())
+
+
+const studentRouter = require('./routers/students')
+app.use('/students',studentRouter)    
+
+app.listen(9000, () => {
+    console.log('server started')
+})
